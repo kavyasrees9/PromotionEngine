e16c9d4444e30668312f2e57a98eebd8da8f02a6
Kavyasree Sadara <kavyasree.skr@gmail.com>
2022-01-05 12:24:12 +0000

    implemented setup test promotions method to store the promotion id,title,promotion type, fixed price values for active promotions in junit test class.

--- src/test/java/com/promotionengine/CartPriceCalculatorTest.java
+++ src/test/java/com/promotionengine/CartPriceCalculatorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -18,6 +19,8 @@ import com.promotionengine.data.IProductsProvider;
 import com.promotionengine.data.IPromotionsProvider;
 import com.promotionengine.model.Cart;
 import com.promotionengine.model.Product;
+import com.promotionengine.model.Promotion;
+import com.promotionengine.model.PromotionType;
 
 @SpringBootTest
 @TestInstance(Lifecycle.PER_CLASS)
@@ -31,32 +34,57 @@ public class CartPriceCalculatorTest {
 
 	@Autowired
 	ICartPriceCalculator cartPriceCalculator;
-	
+
 	@BeforeAll
 	void init() {
 		// setup test products
 		setupTestProducts();
+		// setup test promotions
+		setupTestPromotions();
 	}
 
 	private void setupTestProducts() {
 		// TODO Auto-generated method stub
-		List<Product> testProducts= new ArrayList<Product>() {
+		List<Product> testProducts = new ArrayList<Product>() {
 			{
-				add(new Product("A",50));
-				add(new Product("B",30));
-				add(new Product("C",20));
-				add(new Product("D",15));
+				add(new Product("A", 50));
+				add(new Product("B", 30));
+				add(new Product("C", 20));
+				add(new Product("D", 15));
 			}
 		};
 		// setup get products mock
 		when(productsProvider.GetProducts()).thenReturn(testProducts);
-		testProducts.forEach((Product p)->{
+		testProducts.forEach((Product p) -> {
 			when(productsProvider.GetProduct(p.getSKU_ID())).thenReturn(p);
 		});
-		
+
 	}
 
+	private void setupTestPromotions() {
+		// TODO Auto-generated method stub
+		List<Promotion> testPromotions= new ArrayList<Promotion>();
+		testPromotions.add(new Promotion(1, "3 of A's for 130", new HashMap<String, Integer>(){
+			{
+				put("A",3);
+			}
+		}, PromotionType.MULTIPLEITEMSOFSKU, 130d, null));
+		testPromotions.add(new Promotion(2, "2 of B's for 45", new HashMap<String, Integer>() {
+			{
+				put("B", 2);
+			}
+		}, PromotionType.MULTIPLEITEMSOFSKU, 45d, null));
+		testPromotions.add(new Promotion(3, "C & D for 30", new HashMap<String, Integer>() {
+			{
+				put("C", 1);
+				put("D", 1);
+			}
+		}, PromotionType.COMBINEDSKUS, 30d, null));
+		when(promotionsProvider.GetPromotions()).thenReturn(testPromotions);
 
+		
+		
+	}
 
 	@Test
 	void ScenarioA() {
