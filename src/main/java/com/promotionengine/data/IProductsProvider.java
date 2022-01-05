6086e10357db8a90580a48585969ff1cdb73ef29
Kavyasree Sadara <kavyasree.skr@gmail.com>
2022-01-05 11:44:41 +0000

    Added data provider method  declarations in promotion provider and product provider interfaces.

--- src/main/java/com/promotionengine/data/IProductsProvider.java
+++ src/main/java/com/promotionengine/data/IProductsProvider.java
@@ -1,5 +1,11 @@
 package com.promotionengine.data;
 
+import java.util.List;
+
+import com.promotionengine.model.Product;
+
 public interface IProductsProvider {
 
+	List<Product> GetProducts();
+	Product GetProduct(String sku);
 }
--- src/main/java/com/promotionengine/data/IPromotionsProvider.java
+++ src/main/java/com/promotionengine/data/IPromotionsProvider.java
@@ -1,5 +1,11 @@
 package com.promotionengine.data;
 
+import java.util.List;
+
+import com.promotionengine.model.Promotion;
+
 public interface IPromotionsProvider {
+	
+	List<Promotion> GetPromotions();
 
 }
